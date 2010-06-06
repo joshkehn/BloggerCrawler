bcc8d60918c446c8c7d91ed9d2161f5b5a1875dd
Josh Kehn <Josh.Kehn@gmail.com>
2010-06-06 01:36:41 -0400

    SQL branch for debug / testing

---
+++ SqlTest.java
@@ -0,0 +1,14 @@
+/**
+* Testing out a MySQL database connection
+*
+* @author 	Joshua Kehn
+*
+* @date		06/06/2010
+*/
+public class SqlTest
+{
+	public static void main(String [] args)
+	{
+		
+	}
+}
